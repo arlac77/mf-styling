63a2367c66e42a43056d38d1a8d5c77bad9a8562
Markus Felten <Markus.Felten@gmx.de>
2023-06-21 13:56:08 +0100

    fix: separate js

---
+++ tests/app/table-sorting.mjs
@@ -0,0 +1,80 @@
+const SORT_NONE = "none";
+const SORT_ASCENDING = "ascending";
+const SORT_DESCENDING = "descending";
+
+const orderByCycle = {
+  [SORT_NONE]: SORT_ASCENDING,
+  [SORT_ASCENDING]: SORT_DESCENDING,
+  [SORT_DESCENDING]: SORT_ASCENDING
+};
+
+export function toggleOrderBy(event) {
+  const node = event.currentTarget;
+  const th = node.parentElement;
+  const table = th.parentElement.parentElement.parentElement;
+
+  const sort = orderByCycle[th.getAttribute("aria-sort")] || SORT_NONE;
+  th.setAttribute("aria-sort", sort);
+
+  sortTable(table, parseInt(th.id), sort, "string");
+
+  for (const peer of th.parentElement.children) {
+    let sort = peer.getAttribute("aria-sort");
+    if (sort) {
+      if (peer !== th) {
+        if (sort !== SORT_NONE) {
+          sort = SORT_NONE;
+          peer.setAttribute("aria-sort", sort);
+        }
+      }
+    }
+  }
+}
+
+export function enableSorting(th) {
+  if (th.getAttribute("aria-sort")) {
+    const button = document.createElement("button");
+    button.setAttribute("class", "alter-sorting");
+    button.setAttribute("aria-label", `sortable ${th.id}`);
+    button.onclick = toggleOrderBy;
+    th.appendChild(button);
+  }
+}
+
+export function sortTable(table, colNum, direction, type) {
+  const tbody = table.getElementsByTagName("tbody")[0];
+
+  let m = 1;
+
+  switch (direction) {
+    case SORT_ASCENDING:
+      m = 1;
+      break;
+    case SORT_DESCENDING:
+      m = -1;
+      break;
+  }
+
+  let compare;
+
+  switch (type) {
+    case "number":
+      compare = (rowA, rowB) =>
+        (rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML) * m;
+      break;
+
+    default:
+      compare = (rowA, rowB) =>
+        rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML
+          ? 1 * m
+          : -1 * m;
+
+      break;
+  }
+
+  let rows = Array.from(tbody.rows);
+
+  rows.sort(compare);
+
+  tbody.append(...rows);
+}
