ad013ce0ee3bc8573a118cdd54efad63f6e1c764
Markus Felten <Markus.Felten@gmx.de>
2025-12-07 18:55:55 +0000

    fix: actual tests

---
+++ tests/playwright/visual.spec.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test("tables", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  await page.getByRole("link", { name: "Tables" }).click();
+
+  await page.screenshot({ path: "test-results/screenshots/tables.png" });
+
+  await page
+    .getByRole("table")
+    .filter({
+      hasText:
+        "Column A Column B Column C Column D Cell D 1 Cell B 1 Cell C 1 Cell A 1 Cell A"
+    })
+    .getByLabel("sortable 0")
+    .click();
+  await expect(
+    page.getByRole("cell", { name: "Cell A" }).first()
+  ).toBeVisible();
+});
